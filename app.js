1532088c7049e4e9743c3e0184aa874707f5981b
Hossam Galal <69807707+HossamHR@users.noreply.github.com>
2021-05-26 14:26:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    //code
+    apiKey: "AIzaSyDw4sHWyPhkEGhniDh9S-zDyNljJE2ZAFc",
+    authDomain: "nonrox-2.firebaseapp.com",
+    databaseURL: "https://nonrox-2-default-rtdb.firebaseio.com",
+    projectId: "nonrox-2",
+    storageBucket: "nonrox-2.appspot.com",
+    messagingSenderId: "944147559503",
+    appId: "1:944147559503:web:e7528bb29d90068e0f263a",
+    measurementId: "G-09QGQXK7P5"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -12,7 +19,7 @@ var firebaseConfig = {
         Notification.requestPermission().then(permission=>{
           console.log(permission)
           if(permission == "granted"){
-              messaging.getToken({vapidKey:"public_key"}).then(currentToken=>{
+              messaging.getToken({vapidKey:"BGedTpHFyrb9VdM1g75QfRYo2-qyZZ7SEQDmoIBoWmRAlEcGz5UZVSrPq-HtCSnl3oeyLzkk27WFxUY6qIugVYs"}).then(currentToken=>{
                   console.log(currentToken)
                   document.getElementById('showToken').innerHTML = currentToken
 
@@ -48,7 +55,7 @@ var firebaseConfig = {
       const options = {
           method: "POST",
           headers: new Headers({
-            Authorization:"key=YOUR_SERVER_KEY",
+            Authorization:"key=AAAA29OUdE8:APA91bFN-aVpkmBzDonib3vggRvBUwSukqFLMMZaXjYeNuf_wJLbmNXLxqthxAhu2GTrWj_ov1TedZtJz33kgDhyyRpopN8vlmwSMkrlnyw1SD-GxflbQ0o6zhsGNQB8FZAXgc6Wj0eE",
             "Content-Type":"application/json"
           }),
           body:JSON.stringify(body)
